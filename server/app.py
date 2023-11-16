aa61986ba6c7c5537974402347fdcdf6d038a509
anissalopez <anissa965@gmail.com>
2023-11-16 10:01:39 -0500

    working with apis

--- server/app.py
+++ server/app.py
@@ -14,12 +14,49 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    db_messages = Message.query.all()
+    if db_messages:
+        if request.method == 'GET':
+            messages = [message.to_dict() for message in db_messages]
+            response = make_response(messages, 200)
+            return response
+        elif request.method == 'POST':
+            message = Message(username=request.json.get("username"), 
+                               body=request.json.get("body"))
+            db.session.add(message)
+            db.session.commit()
+            message_dict = message.to_dict()
+            response = make_response(message_dict, 201)
+            return response
+    else:
+        message = {'message': 'error retrieving messages from db'}
+        return make_response(message, 200)
+    
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def messages_by_id(id): 
+    message = Message.query.filter_by(id=id).first()
+    if message:
+        if request.method == 'PATCH':
+            for attr in request.json:
+                setattr(message, attr, request.json.get(attr))
+            db.session.add(message)
+            db.session.commit()
+            message_dict = message.to_dict()
+            response = make_response(message_dict, 200)
+            return response
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+            message = {"message": "message succesfully deleted"}
+            response = make_response(message, 200)
+    else:
+        body = {"Error": f"error retreiving message at {id}"}
+        response = make_response(body, 200)
+        return response
+
     return ''
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -8,7 +8,24 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+
+# "body": String.
+# "username": String.
+# "created_at": DateTime.
+# "updated_at": DateTime.
+# Don't forget to add default values for "created_at" and "updated_at"!
+# (Hint - we discussed this in the Phase 3 Many-to-Many Relationships reading and gave an 
+#  example in the Phase 4 Building a Get API Reading.)
+
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'Username: {self.username}, body:{self.body}'
+
